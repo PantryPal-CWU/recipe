f61d6606dd06c7eb2886f234669caac6306bfe4e
Kirsten Boyles <kboyles138@gmail.com>
2021-01-23 12:25:03 -0800

    nav bar start

---
+++ react-pantrypal/src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React, {useState} from 'react'
+
+function Navbar() {
+    return (
+        <>
+            <nav className="navbar">
+                <div className="navbar-container">
+                    
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar
+
+
