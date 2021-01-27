47e72d206704a4ed7ba03c5089f8da33a4d27332
Natalia Gonzalez <natalia.gonzalez@gmail.com>
2021-01-27 13:35:19 -0800

    added comments to ingredientsform.js

--- src/components/Ingredients&Pantry/IngredientsForm.js
+++ src/components/Ingredients&Pantry/IngredientsForm.js
@@ -1,13 +1,19 @@
 import React from 'react';
 
+//Search Bar function
 const SearchBar = ({keyword, setKeyword}) => {
+    //Search bar styling
+    //could have done it in a .css file but I'm still learning how to do that
     const BarStyling = {width:"20rem",background:"#F2F1F9A", borderborder:"none", padding:"0.5rem"};
+    //return the search bar styling and intialize the value to the keyword data variable
     return(
         <input
         style={BarStyling}
         key="random1"
         value={keyword}
         placeholder={"Search for an ingredient"}
+        //places the the keyword entered in the search bar
+        //into the setKeyword variable using the onChange data type
         onChange={(e) => setKeyword(e.target.value)}
         />
     );
