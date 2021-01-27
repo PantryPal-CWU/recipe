65eb61bfd509380d4ab330a6e10b6a89b89350f1
Lucas Keizur <Lucasckz@yahoo.com>
2021-01-26 16:12:52 -0800

    About.js 1/26/2021 16:12

---
+++ src/components/About/About.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+ 
+class Main extends Component {
+  render() {
+    return (
+        <div>
+          <h1>About Us!</h1>
+          
+        </div>
+    );
+  }
+}
+
+export default About;
