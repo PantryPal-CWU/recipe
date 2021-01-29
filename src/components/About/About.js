0d4c7f82bc83978f177daa7300e4fe842474125b
Lucas Keizur <Lucasckz@yahoo.com>
2021-01-29 14:29:58 -0800

    About 1/29/2021 2:29pm

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,12 +1,48 @@
+/*
+File: About.js
+?: Design for elements in About/Contact. 
+*/
+import './About.css';
+//import GlobalStyle from '../styles/Global';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import styled from 'styled-components';
 
 class About extends React.Component {
+
   render() {
+
     return (
       <div>
-        <br></br>
-        <p> HelloWorld! </p>
+
+        <div className="title" >
+          <h1> About Us! </h1>
+        </div>
+
+        <div className="content">
+            <p1 > Lucas </p1>
+        </div>
+
+          <div className="content">
+            <p1> Kirsten </p1>
+          </div>
+
+          <div className="content">
+            <p1 > Nate </p1>
+          </div>
+
+          <div className="content">
+            <p1 > Alek </p1>
+          </div>
+
+          <div className="content">
+            <p1 > Natalia </p1>
+          </div>
+
+          <div className="content">
+            <p1 > Jordan </p1>
+          </div>
+
       </div>
     )
   }
