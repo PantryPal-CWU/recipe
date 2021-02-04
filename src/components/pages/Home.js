afcd75ab2f9a6e35667957c2fdfcbd3c2f7331f0
Kirsten B <kboyles138@gmail.com>
2021-02-04 12:52:10 -0800

    started profile

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -9,24 +9,25 @@ import img1 from "../images/cooking-5880136_1920.jpg";
 import img2 from "../images/food-2068217_1920.jpg";
 import img3 from "../images/mediterranean-cuisine-2378758_1920.jpg";
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-export default function App() {
-  return (
-    <>
-      <Headers>
+/*<Headers>
         <h1>
           Welcome to PantryPal
             </h1>
-      </Headers>
+      </Headers>*/
+export default function App() {
+  return (
+    <>
+      
       <div className='container-fluid'>
         <Carousel>
           <Carousel.Item>
             <img
               className="d-block w-100"
-              src={img1}
+              src={img3}
               alt="First slide"
             />
             <Carousel.Caption>
+              <h1> Welcome to PantryPal</h1>
               <h3>First slide label</h3>
               <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
             </Carousel.Caption>
@@ -46,7 +47,7 @@ export default function App() {
           <Carousel.Item>
             <img
               className="d-block w-100"
-              src={img3}
+              src={img1}
               alt="Third slide"
             />
 
---
+++ src/components/pages/Profile.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import styled from 'styled-components';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Form from 'react-bootstrap/Form';
