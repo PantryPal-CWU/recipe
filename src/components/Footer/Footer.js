d035ad66a4a5760346543bfdbd10e6285ebe6302
Lucas Keizur <Lucasckz@yahoo.com>
2021-02-05 16:05:02 -0800

    Footer Adjustment

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,7 +8,15 @@ export function Footer() {
 
     return (
         <div className="footPage">
-            <h>Hello Everyone (Footer) </h>
+            <div className="footCont">Hello Everyone (Footer) </div>
+
+            <div className="footRow">
+                <div className="footCont">One-Link</div>
+                <div className="footCont">Two-Link</div>
+                <div className="footCont">three-Link</div>
+            </div>
+            <div className="footCont">Four - Contents</div>
+
         </div>
     )
 }
