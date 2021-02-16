ccf593e4d4d99d9bef4ac8ca702e4e5f3e3ab172
AlekMichael123 <alekmichael2345@gmail.com>
2021-02-16 01:15:57 -0800

    Fixed Authenticate. Should work properly now

--- src/components/Login/Authenticate.js
+++ src/components/Login/Authenticate.js
@@ -9,18 +9,22 @@ export default function Authenticate(props) {
         const fetchData = async () => {
             //Will have to change sometime
             const res = await fetch(`http://localhost:4001/authenticate?email=${props.email}&password=${props.password}`)
-            const data = await res.json();
-            if(!data) {
-                alert("Incorrect login");
-            } else {
-                toggleLoginStatus(props.email);
-            }
+            const data = await res.json().then(data => {
+                if(!data) {
+                    alert("Incorrect login");
+                    props.setCheck(false);
+                    window.location.reload(false);
+                } else {
+                    toggleLoginStatus(props.email);
+                    alert("Welcome!");
+                    window.location.reload(false);
+                }
+            });
+            
             
         };
 
         fetchData();
-        props.setCheck(false);
-        window.location.reload(false);
     }, []);
 
     return <></>;
