335247c5ee97663b1b97fec49e869508694250f2
Natalia Gonzalez <natalia.gonzalez@gmail.com>
2021-02-25 10:32:41 -0800

    updating file in expressServer

--- expressServer/public/components/Ingredients/Ingredients.js
+++ expressServer/public/components/Ingredients/Ingredients.js
@@ -3,25 +3,19 @@ import React, {useState, useEffect} from 'react';
 //import SearchBar from './SearchForm';
 
 const SearchPage = () => {
-
-    const [searchTerm, setSearchTerm] = useState('');
-
-    const updateSearchTerm = async (searchTerm) =>{
-       setSearchTerm(searchTerm); 
-    }
-
+    const [searchTerm, setSearchTerm] = useState("");
+  
+  //   const editSearchTerm = (e) => {
+        
+  //   }
+  
     return(
-            <div style={{textAlign: 'center', paddingBottom: '30px'}}>
-                <input
-                    type='text'
-                    value={searchTerm}
-                    onChange= {updateSearchTerm}
-                    placeholder='Search for a recipe'
-                />
-                <br></br>
-                <h3>Search Results: </h3>
-            </div>
+        <div style={{textAlign: 'center', paddingBottom: '40px', marginTop: '60px'}}>
+            <input type='text' value={searchTerm} onChange={e => setSearchTerm(e.target.value)} placeholder = 'Search for recipe' />
+            <br></br>
+            <h3>Search Results: {searchTerm} </h3>
+        </div>
     );
-}
-
-export default SearchPage;
+  }
+  
+  export default SearchPage;
