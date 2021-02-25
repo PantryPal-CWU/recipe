d0dd417efcf16e803f9616337474f7d173ac805e
Natalia Gonzalez <natalia.gonzalez@gmail.com>
2021-02-25 02:24:58 -0800

    changes to search bar

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,25 +1,31 @@
 import { render } from '@testing-library/react';
 import React, {useState, useEffect} from 'react';
-import SearchBar from './SearchForm';
+//import SearchBar from './SearchForm';
 
+class SearchPage extends Component{
 
+    state = {
+        searchTerm = ''
+    }
 
-const SearchPage = () => {
-  state = {
-      searchTerm = ''
-  }
+    editSearchTerm = (e) =>{
+        this.state({
+            searchTerm: e.target.value
+        });
+    }
 
-  editSearchTerm = (e) =>{
-      this.state({searchTerm: e.target.value});
-  }
-
-  return(
-      <div style={{textAlign: 'center', paddingBottom: '40px'}}>
-          <input type='text' value={this.state.searchTerm} onChange={this.editSearchTerm} placeholder = 'Search for recipe'/>
-          <br></br>
-          <h3>Search Results: </h3>
-      </div>
-  );
+    render(){
+        return(
+            <div style={{textAlign: 'center', paddingBottom: '30px'}}>
+                <input
+                    type='text'
+                    value={this.state.searchTerm}
+                    onChange={this.editSearchTeam}
+                    placeholder='Search for a recipe'
+                />
+                <br></br>
+                <h3>Search Results: </h3>
+            </div>
+        );
+    }
 }
-
-export default SearchPage;
