05f4dd65c7d0ccd94618803beeba37820eac705d
Natalia Gonzalez <natalia.gonzalez@gmail.com>
2021-03-02 03:07:34 -0800

    setting up the api requests

--- expressServer/public/components/Ingredients/Ingredients.js
+++ expressServer/public/components/Ingredients/Ingredients.js
@@ -1,13 +1,12 @@
 import { render } from '@testing-library/react';
 import React, {useState, useEffect} from 'react';
 //import SearchBar from './SearchForm';
+const unirest = require('unirest');
+const API_KEY = "b5d4e74876msh2a858525a9d8cb9p16e416jsn956d33b8866a";
+let requestString = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/search" + "?q=";
 
 const SearchPage = () => {
     const [searchTerm, setSearchTerm] = useState("");
-  
-  //   const editSearchTerm = (e) => {
-        
-  //   }
   
     return(
         <div style={{textAlign: 'center', paddingBottom: '40px', marginTop: '60px'}}>
