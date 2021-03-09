b7348691ca0472eacfa669425dde82d5fa07c6a7
NATE GOODMAN <nategoodman4241@gmail.com>
2021-03-08 18:09:54 -0800

    once again fixing package.json issue

--- setupProxy.js
+++ setupProxy.js
@@ -1,6 +1,16 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
+    app.use(
+      '/api',
+      createProxyMiddleware({
+        target: 'http://localhost:4001',
+        changeOrigin: true,
+      })
+    );
+  };
+
+module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
