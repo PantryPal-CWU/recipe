5c68c920d6c27255bffbd1c9ba94b90adad114cd
NATE GOODMAN <nategoodman4241@gmail.com>
2021-03-08 17:24:17 -0800

    new way to proxy sites

---
+++ setupProxy.js
@@ -0,0 +1,11 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'http://www.recipepuppy.com',
+      changeOrigin: true,
+    })
+  );
+};
