9f5f39f3b858b6620b79aebacca98f06c6a1b37f
Kirsten Boyles <kboyles138@gmail.com>
2021-03-08 22:57:54 -0800

    Revert "once again fixing package.json issue"
    This reverts commit b7348691ca0472eacfa669425dde82d5fa07c6a7.

--- setupProxy.js
+++ setupProxy.js
@@ -1,16 +1,6 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
-    app.use(
-      '/api',
-      createProxyMiddleware({
-        target: 'http://localhost:4001',
-        changeOrigin: true,
-      })
-    );
-  };
-
-module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
